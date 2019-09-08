a95175cc244a35ccf9b0a346d6439ee01a53f4a5
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-09-08 13:09:02 +0300

    Calculator exercise completed

--- JS Essentials/DomAndObjects/Exercise/08. Numpad-Calculator/solution.js
+++ JS Essentials/DomAndObjects/Exercise/08. Numpad-Calculator/solution.js
@@ -1,4 +1,34 @@
 function solve() {
-
+    let expression = document.getElementById("expressionOutput");
+    let result = document.getElementById("resultOutput");
+let regexExpression = /([0-9]+[*\/+-]{1}[0-9]+)/g;
+    document.getElementsByClassName("clear")[0].addEventListener("click",function(){
+        expression.innerHTML = "";
+        result.innerHTML = "";
+    });
+    let buttons = document.getElementsByClassName("keys")[0].getElementsByTagName("button");
+    for(const btn of buttons){
+        btn.addEventListener("click",function(){
+            let value = this.value
+            if(value == "="){
+                try{
+                    if(regexExpression.test(expression.innerHTML.replace(/\s+/g,"")) == false){
+                        throw SyntaxError("Missing operands");
+                    }
+                    result.innerHTML = eval(expression.innerHTML);
+                }catch(SyntaxError){
+                    result.innerHTML = "NaN";
+                }
+                
+                return;
+            }else if(value == "+" || value == "-" || value == "*" || value == "/"){
+                expression.innerHTML+=" "+this.value+" ";
+            }else{
+                expression.innerHTML+=this.value;
+            }
+           
+            
+        });
+    }
     //TODO...
 }
