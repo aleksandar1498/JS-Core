05c1127cf57c0596f0f6a5d8b140135d5e3154a4
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-11 17:52:47 +0300

    Replace Word Exercise

--- JS Essentials/StringAndRegeExp/Lab/04_Replace-a-Certain-Word/solution.js
+++ JS Essentials/StringAndRegeExp/Lab/04_Replace-a-Certain-Word/solution.js
@@ -1,4 +1,16 @@
 function solve() {
+ let replacingWord = document.getElementById("word").value;
+ let content = document.getElementById("text").value;
+ console.log(replacingWord);
+ let words = JSON.parse(content);
+ let wordToReplace =words[0].split(/\s+/)[2];
+ let pattern = new RegExp(""+wordToReplace+"","gi");
+ let result = document.getElementById("result");
+ for(let word of words){
+	let p = document.createElement("p");
+	p.innerHTML = word.replace(pattern,replacingWord);
+	result.appendChild(p);
+}
 
-    //TODO...
+
 }
