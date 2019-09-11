8612933becd82950047eac83c018d02adab5ea92
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-11 16:21:48 +0300

    Ascii Converter

--- JS Essentials/StringAndRegeExp/Lab/02_Find-Ascii-Equivalent/solution.js
+++ JS Essentials/StringAndRegeExp/Lab/02_Find-Ascii-Equivalent/solution.js
@@ -1,4 +1,25 @@
 function solve() {
-
-  //TODO...
+	let content = document.getElementById("text").value;
+	content = content.trim().split(/\s+/g);
+	console.log(content);
+	let result = document.getElementById("result");
+	
+	let resultWord ="";
+	for(const word of content){
+		if(isNaN(Number(word))){
+			let encodings = word.split('');
+			for(let i=0;i<encodings.length;i++){
+				encodings[i] = encodings[i].charCodeAt(0);
+			}
+			let p = document.createElement("p");
+			p.innerHTML+=`${encodings.join(" ")}`;
+			result.appendChild(p);
+			
+		}else{
+			resultWord+=String.fromCharCode(Number(word));
+		}
+	}
+	let p = document.createElement("p");
+	p.innerHTML+=resultWord;
+	result.appendChild(p);
 }
