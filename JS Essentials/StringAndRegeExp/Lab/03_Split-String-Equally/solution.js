81bae9dd9a6250fbeebd42ff1a8c4a709be4d660
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-11 16:57:57 +0300

    Splitter Exercise

--- JS Essentials/StringAndRegeExp/Lab/03_Split-String-Equally/solution.js
+++ JS Essentials/StringAndRegeExp/Lab/03_Split-String-Equally/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-    //TODO...
+    let textArea = document.getElementById("text").value;
+	let splitting = Number(document.getElementById("number").value);
+	let result = document.getElementById("result");
+	if(textArea.length%splitting != 0){
+		let reminder = splitting - textArea.length%splitting;
+		textArea+=textArea.substr(0,reminder);
+	}
+	let splittedWord = [];
+		for(let i = 0;i < textArea.length ;i+=splitting){
+			splittedWord.push(textArea.substr(i,splitting));
+		}
+		result.innerHTML = splittedWord.join(" ");
 }
