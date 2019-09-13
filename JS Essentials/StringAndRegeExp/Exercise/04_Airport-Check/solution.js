d7980a8546f09197420bec748ba1a5e8a40dc328
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-13 15:04:55 +0300

    Regex For airport completed

--- JS Essentials/StringAndRegeExp/Exercise/04_Airport-Check/solution.js
+++ JS Essentials/StringAndRegeExp/Exercise/04_Airport-Check/solution.js
@@ -1,4 +1,6 @@
 function solve() {
-
-    //TODO...
+	let data = document.getElementById("string").value;
+ let reg = /.*?(?<name> [A-Z][a-z]*\-[A-Z][a-z]*(\.[A-Z][a-z]*)? ).*?(?<airport> [A-Z]{3}\/[A-Z]{3} ).*?(?<flight> [A-Z]{1,3}[0-9]{1,5} ).*?(?<company>\- [A-Z][a-z]+\*[A-Z][a-z]+ )/;
+ let match = data.match(reg).groups;
+ console.log(match.name);
 }
