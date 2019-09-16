078c77be680d930a7f4c464a3b9f3f25ea163462
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-16 20:26:06 +0300

    Encoded receiver

--- JS Essentials/DomAndObjects/Exercise/08. Encode-and-Decode-Messages/app.js
+++ JS Essentials/DomAndObjects/Exercise/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,22 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+	let buttons = document.getElementsByTagName("button");
+	let textareas = document.getElementsByTagName("textarea");
+	buttons[0].addEventListener('click',function(){
+		let textToEncode = textareas[0].value;
+		textareas[0].value = "";
+		textareas[1].value = encodeText(textToEncode);
+	});
+	buttons[1].addEventListener('click',function(ev){
+		
+		let textToDecode = textareas[1].value;
+		console.log(textToDecode);
+		textareas[1].value = decodeText(textToDecode);
+	});
+	
+    function encodeText(word){
+		return word.split('').map(c => String.fromCharCode(c.charCodeAt(0)+1)).join('');
+	}
+	function decodeText(word){
+		return word.split('').map(c => String.fromCharCode(c.charCodeAt(0)-1)).join('');
+	}
 }
