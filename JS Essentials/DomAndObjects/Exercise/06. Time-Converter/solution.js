69f61704d9b26f863b88d8186145222fe32c9188
aleksandar1498 <aleksandar.fs@gmail.com>
2019-09-16 16:40:13 +0300

    new Exercises with DOM

--- JS Essentials/DomAndObjects/Exercise/06. Time-Converter/solution.js
+++ JS Essentials/DomAndObjects/Exercise/06. Time-Converter/solution.js
@@ -1,4 +1,35 @@
 function attachEventsListeners() {
+let daysBtn = document.getElementById("daysBtn");
+let hoursBtn = document.getElementById("hoursBtn");
+let minutesBtn = document.getElementById("minutesBtn");
+let secondsBtn = document.getElementById("secondsBtn");
+daysBtn.addEventListener("click",function(ev){
+	let days = Number(ev.target.previousElementSibling.value);
+	hoursBtn.previousElementSibling.value = days * 24;
+	minutesBtn.previousElementSibling.value = days * 24 * 60;
+	secondsBtn.previousElementSibling.value = days * 24 * 60 * 60;
+});
 
-    console.log('TODO:...');
+hoursBtn.addEventListener("click",function(ev){
+	let hours = Number(ev.target.previousElementSibling.value);
+	console.log(hours / 24);
+	daysBtn.previousElementSibling.value = (hours / 24);
+	minutesBtn.previousElementSibling.value = hours * 60;
+	secondsBtn.previousElementSibling.value = hours * 60 * 60;
+});
+
+minutesBtn.addEventListener("click",function(ev){
+	let minutes = Number(ev.target.previousElementSibling.value);
+	daysBtn.previousElementSibling.value = (minutes / (24*60)).toFixed(0);
+	hoursBtn.previousElementSibling.value = (minutes / 60).toFixed(0);
+	secondsBtn.previousElementSibling.value = minutes * 60;
+});
+
+secondsBtn.addEventListener("click",function(ev){
+	let seconds = Number(ev.target.previousElementSibling.value);
+	daysBtn.previousElementSibling.value = (seconds / (3600*24)).toFixed(0);
+	hoursBtn.previousElementSibling.value = (seconds / 3600).toFixed(0);
+	minutesBtn.previousElementSibling.value = (seconds / 60).toFixed(0);
+	
+});
 }
