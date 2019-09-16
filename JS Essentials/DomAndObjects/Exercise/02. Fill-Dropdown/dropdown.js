d9ed52c5ee79acc03ddf799f980aaba79b303380
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-09-16 13:11:10 +0300

    New Exercise Completed

--- JS Essentials/DomAndObjects/Exercise/02. Fill-Dropdown/dropdown.js
+++ JS Essentials/DomAndObjects/Exercise/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemText = document.getElementById("newItemText");
+    let itemValue = document.getElementById("newItemValue");
+    let o = document.createElement("option");
+    o.textContent = itemText.value;
+    o.value = itemValue.value;
+    itemText.value = "";
+    itemValue.value = "";
+    document.getElementById("menu").appendChild(o);
 }
