9dd2ee2ab4b057f21fad3f43367d705d26648c97
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-09-16 13:02:34 +0300

    Substraction Exercise With DOM

---
+++ JS Essentials/DomAndObjects/Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,6 @@
+function subtract() {
+    let fNumber = Number(document.getElementById("firstNumber").value);
+    let sNumber = Number(document.getElementById("secondNumber").value);
+    document.getElementById("result").innerHTML = fNumber-sNumber;
+
+}
