90ea7105dfa28166e46e430baa60693954e3fb69
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-09-23 19:51:28 +0300

    Exam Exercise

--- JS Advanced/Exam Preparation/Heroes/scripts/solution.js
+++ JS Advanced/Exam Preparation/Heroes/scripts/solution.js
@@ -7,15 +7,23 @@ function solve() {
 
    document.getElementById("kingdom").getElementsByTagName("button")[0].addEventListener("click", function (ev) {
       let current = ev.target;
-      let kingdom = current.previousElementSibling.previousElementSibling.value;
-      let king = current.previousElementSibling.value;
-      if (king.length < 2) {
+      let kingdom = current.previousElementSibling.previousElementSibling;
+
+      let king = current.previousElementSibling;
+      let invalid = false;
+      if (!kingdoms[kingdom.value.toLowerCase()]) {
+         kingdom.value = "";
+         invalid = true;
+      }
+      if (king.value.length < 2) {
+
+         king.value = "";
+         invalid = true;
+      }
+      if (invalid) {
          return;
       }
-      if (!kingdoms[kingdom.toLowerCase()]) {
-         return;
-      }
-      rebuild(kingdoms[kingdom.toLowerCase()], king);
+      rebuild(kingdoms[kingdom.value.toLowerCase()], king.value);
 
    });
    function rebuild(kingdom, king) {
@@ -53,28 +61,33 @@ function solve() {
    }
    document.getElementById("characters").getElementsByTagName("div")[3].getElementsByTagName("button")[0].addEventListener("click", function (ev) {
       let current = ev.target;
-      let kingdom = current.previousElementSibling.value;
-      let character = current.previousElementSibling.previousElementSibling.value;
+      let invalid = false;
+      let kingdom = current.previousElementSibling;
+      let character = current.previousElementSibling.previousElementSibling;
       let characterType = Array.from(document.getElementById("characters").getElementsByTagName("input")).find(i => i.name == "characterType" && i.checked == true);
       if (!characterType) {
-         console.log("character is not selected");
-         return;
-      } else if (character.length < 2) {
-         console.log("Character invalid");
-         return;
-      } else if (!kingdoms[kingdom.toLowerCase()] || kingdoms[kingdom.toLowerCase()].style.display == 'none') {
-         console.log("Kingdom is not rebuild");
-         return;
-      } else {
-
-         kingdoms[kingdom.toLowerCase()].getElementsByClassName("armyOutput")[0].innerHTML += " " + character;
-         console.log(characterType.value);
+         invalid = true;
+      }
+      if (character.value.length < 2) {
+         character.value = "";
+         invalid = true;
+      }
+      if (!kingdoms[kingdom.value.toLowerCase()] || kingdoms[kingdom.value.toLowerCase()].style.display == 'none') {
+         kingdom.value = "";
+         invalid = true;
       }
 
-      let charactersCounts = document.getElementsByTagName("fieldset")[0].getElementsByTagName("p");
+      if (invalid) {
+         return;
+      }
+      kingdoms[kingdom.value.toLowerCase()].getElementsByClassName("armyOutput")[0].innerHTML += character.value + " ";
+
+
+
+      let charactersCounts = kingdoms[kingdom.value.toLowerCase()].getElementsByTagName("fieldset")[0].getElementsByTagName("p");
       let position = -1;
-      switch (characterType) {
-         case "tanks":
+      switch (characterType.value) {
+         case "tank":
             position = 0;
             break;
          case "fighter":
@@ -84,14 +97,70 @@ function solve() {
             position = 2;
             break;
       }
+
       let currentVal = Number(charactersCounts[position].innerHTML.match(/[0-9]+/));
       currentVal++;
-      charactersCounts[position].innerHTML = charactersCounts[position].innerHTML.replace(/[0-9]+/g,currentVal);
+      charactersCounts[position].innerHTML = charactersCounts[position].innerHTML.replace(/[0-9]+/g, currentVal);
 
    });
 
 
+   document.getElementById("actions").getElementsByTagName("button")[0].addEventListener("click", (ev) => {
+      let current = ev.target;
+      let invalid = false;
+      let attacker = current.previousElementSibling.previousElementSibling;
+      let defender = current.previousElementSibling;
+      if (!kingdoms[attacker.value.toLowerCase()] || kingdoms[attacker.value.toLowerCase()].style.display == 'none') {
+         attacker.value = "";
+         invalid = true;
+      }
+      if (!kingdoms[defender.value.toLowerCase()] || kingdoms[defender.value.toLowerCase()].style.display == 'none') {
+         defender.value = ""; 
+         invalid = true;
+      }
+      if (invalid) {
+         return;
+      }
+      if (getTotalAttack(kingdoms[attacker.value.toLowerCase()]) > getTotalDefense(kingdoms[defender.value.toLowerCase()])) {
+         kingdoms[defender.value.toLowerCase()].getElementsByTagName("h2")[0].innerHTML = kingdoms[attacker.value.toLowerCase()].getElementsByTagName("h2")[0].innerHTML;
+      }
+
+
+   });
+   function getTotalAttack(attacker) {
+      let paragraphs = attacker.getElementsByTagName("fieldset")[0].getElementsByTagName("p");
+      let totalAttack = 0;
+      for (let p of paragraphs) {
+         totalAttack += getAttacks()[p.innerHTML.match(/[A-Z]+/g)[0].toLowerCase()] * Number(p.innerHTML.match(/[0-9]+/g));
+      }
+      return totalAttack;
+
+   }
+   function getTotalDefense(defender) {
+      let paragraphs = defender.getElementsByTagName("fieldset")[0].getElementsByTagName("p");
+      let totalDefense = 0;
+      for (let p of paragraphs) {
+         totalDefense += getDefenses()[p.innerHTML.match(/[A-Z]+/g)[0].toLowerCase()] * Number(p.innerHTML.match(/[0-9]+/g));
+      }
+      return totalDefense;
+   }
+
+   function getDefenses() {
+      return {
+         "mages": 30,
+         "fighters": 50,
+         "tanks": 80
+      };
+   }
+   function getAttacks() {
+      return {
+         "mages": 70,
+         "fighters": 50,
+         "tanks": 20
+      };
+   }
 }
 solve();
+
 
 
