f63855e4f111f2bf449a238122789b0d65ecafd3
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-11-06 20:50:54 +0200

    Bus Schedule Implemented

--- JS Apps/REST Services & AJAX/Exercise/02.BUS-SCHEDULE/app.js
+++ JS Apps/REST Services & AJAX/Exercise/02.BUS-SCHEDULE/app.js
@@ -1,29 +1,58 @@
 function solve() {
-    const infoBox = document.querySelector("#id span");
+    const infoBox = document.querySelector("#info span");
     const departBtn = document.getElementById('depart');
     const arriveBtn = document.getElementById('arrive');
-    let [current,nextStopId] = ['',''];
+    let [currentName, nextStopId] = [undefined, 'depot'];
     function depart() {
-        fetch(`https://judgetests.firebaseio.com/schedule/{currentId}.json`)
-        toggleButtons(departBtn,arriveBtn);
+        fetch(`https://judgetests.firebaseio.com/schedule/${nextStopId}.json`)
+            .then((res) => {
+             
+                if (res.status == 200) {
+                    return res.json();
+                } else {
+                    throw new Error('error');
+                }
+
+            })
+            .then((data) => {
+             
+                currentName = data.name;
+                nextStopId = data.next;
+                infoBoxContent(`Next stop ${currentName}`);
+                toggleButtons(departBtn, arriveBtn);
+            })
+            .catch(() => {
+                handleError();
+            });
+        
     }
 
     function arrive() {
-        toggleButtons(departBtn,arriveBtn);
+        infoBoxContent(`Arriving at ${currentName}`);
+        toggleButtons(departBtn, arriveBtn);
     }
 
-    function toggleButtons(...btns){
+   
+    function toggleButtons(...btns) {
         for (const btn of btns) {
             toggleState(btn);
         }
     }
-    function toggleState(btn){
-       if(btn.hasAttribute('disabled')){
+    function toggleState(btn) {
+        if (btn.hasAttribute('disabled')) {
             btn.removeAttribute('disabled');
-       } else{
-            btn.setAttribute('disabled','true');
-       }
+        } else {
+            btn.setAttribute('disabled', 'true');
+        }
     }
+    function handleError(){
+        infoBoxContent("Error");
+        departBtn.setAttribute('disabled','true');
+        arriveBtn.setAttribute('disabled','true');
+    }
+    function infoBoxContent(content){
+        infoBox.innerHTML = content;
+    } 
     return {
         depart,
         arrive
