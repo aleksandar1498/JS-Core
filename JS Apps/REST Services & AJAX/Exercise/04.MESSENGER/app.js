0bdf4f482ba7b9acf077621c03902ea3ef3d88ff
aleksandar.fs@gmail.com <aleksandar.fs@gmail.com>
2019-11-07 00:38:23 +0200

    Messenger Implemented

--- JS Apps/REST Services & AJAX/Exercise/04.MESSENGER/app.js
+++ JS Apps/REST Services & AJAX/Exercise/04.MESSENGER/app.js
@@ -1,5 +1,60 @@
 function attachEvents() {
-    console.log('TODO...');
+    let author = document.getElementById("author");
+    let content = document.getElementById("content");
+
+    let submitBtn = document.getElementById("submit");
+    let refreshBtn = document.getElementById("refresh");
+
+    let messages = document.getElementById("messages");
+    submitBtn.addEventListener("click", () => {
+        sendMessage(author.value, content.value);
+    });
+
+    refreshBtn.addEventListener("click", () => {
+        refreshMessages();
+    });
+    function sendMessage(author, content) {
+        
+
+        fetch("https://rest-messanger.firebaseio.com/messanger.json", {
+            method: "post",
+            body: JSON.stringify({
+                author,
+                content,
+                timestamp: Date.now()
+            })
+
+        }).then(
+            (res) => res.json()
+        ).then((data) => {
+            clearData();
+           
+        })
+            .catch((err) => {
+                console.log("err");
+
+            });
+    }
+    function refreshMessages() {
+        fetch("https://rest-messanger.firebaseio.com/messanger.json")
+            .then((res) => res.json())
+            .then((data) => {
+                let sorted = {};
+                Object.keys(data).sort((a,b) => a.timestamp - b.timestamp).forEach(k => sorted[k] = data[k]);
+                for (const key in sorted) {
+                    messages.append(`${data[key].author}:${data[key].content}\n`);    
+                }
+              
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    function clearData(){
+        console.log("called");
+        author.value = '';
+        content.value = '';
+    }
 }
 
 attachEvents();
