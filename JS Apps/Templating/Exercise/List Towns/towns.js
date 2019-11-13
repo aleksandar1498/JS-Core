ba5f60ac22cccb53d3a317d1617f8e79b9e4ed1f
aleksandar1498 <aleksandar.fs@gmail.com>
2019-11-13 20:41:09 +0200

    Templating completed

--- JS Apps/Templating/Exercise/List Towns/towns.js
+++ JS Apps/Templating/Exercise/List Towns/towns.js
@@ -3,11 +3,12 @@
 		let btnLoad = document.getElementById("btnLoadTowns");
 		let townsInput = document.getElementById("towns");
         btnLoad.addEventListener("click",()=>{
+			$("#root").html('');
 			let towns = townsInput.value.split(',')
 			let source = document.getElementById("towns-template").innerHTML;
 			let template = Handlebars.compile(source);
 			let data = template({towns});
-			$("body").append(data);
+			$("#root").html(data);
 		});
     }
 
